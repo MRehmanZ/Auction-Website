ce1a9798c734f16c212ccbe73ea6744a296b5d06
Muhammad Rehman Zulfiquar <mrzulfiquar@gmail.com>
2024-04-15 19:28:38 +0100

    Setup tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
+import logo from "./logo.svg";
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div className="app">
+      <Home />
+      <img src={logo} alt="logo" />
+      <a
+        href="https://docs.clerk.dev/reference/clerk-react"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn more
+      </a>
+    </div>
+  );
 }
 
 export default App;
