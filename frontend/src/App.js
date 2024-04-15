28d136fc0c3ea44eb4c0e8c8788fba1124157a42
Muhammad Rehman Zulfiquar <mrzulfiquar@gmail.com>
2024-04-15 18:47:14 +0100

    Add project planning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-      <div>
-        Hello world
-    </div>
-  );
+  return <div>Hello world</div>;
 }
 
 export default App;
