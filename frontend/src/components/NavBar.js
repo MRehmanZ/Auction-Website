252dc14d7c67480cedc3e6749e23925f43e75464
Muhammad Rehman Zulfiquar <mrzulfiquar@gmail.com>
2024-04-25 22:23:36 +0100

    chore: remove unused import

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { MenuIcon} from "@heroicons/react/outline";
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import Avatar from "@mui/material/Avatar";
